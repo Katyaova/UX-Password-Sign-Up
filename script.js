6865a03fd14e15588e9d9ca59228d55eb6e2facd
Katya <puhhova@hotmail.com>
2026-03-09 23:35:26 +0000

    Basic Rules added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const passwordInput = document.getElementById("password");
+const passwordMessage = document.getElementById("passwordMessage");
+const signUpBtn = document.getElementById("signUpBtn");
 
+function isValidPassword(password) {
+    const hasLength = password.length >=8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasSymbol = /[^A-Za-z0-9]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+}
