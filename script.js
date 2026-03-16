8fd9f227e7f901262cee4bd82121609e4ee7bc9a
Katya <puhhova@hotmail.com>
2026-03-16 15:18:49 +0000

    Added Google Forms tracking to Version A

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 const passwordMessage = document.getElementById("passwordMessage");
 const signUpBtn = document.getElementById("signUpBtn");
+
+const startTime = Date.now();
+let failedAttempts = 0;
+let messageShowedn = 0;
+
+function submitToGoogleForm(data) {
+    fetch("https://docs.google.com/forms/d/e/1FAIpQLSf9f6Ddp7Q17XgFfwefPYaiX0ZdBATKhtH5ZyqGTZYUrBEhcg/formResponse", {
+     method: "POST",
+     mode: "no-cors",
+     headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+     },
+     body: new URLSearchParams({
+        "entry.116586135": data.participantID,
+        "entry.725059856": data.passwordLength,
+        "entry.841442949": data.score,
+        "entry.1808329028": data.crackTime,
+        "entry.1260201898": data.patternName,
+        "entry.1179553110": data.patternYear,
+        "entry.1983954456": data.patternSequence,
+        "entry.1248909794": data.timeTaken
+     })
+    });
+    }
 
 function isValidPassword(password) {
     const hasLength = password.length >= 8;
@@ -13,15 +38,44 @@ function isValidPassword(password) {
 }
 
 signUpBtn.addEventListener("click", function () {
-    
+    const username = usernameInput.value.trim();
     const passwordValue = passwordInput.value;
 
+    const lowerPassword = password.toLowerCase;
+    const lowerUsername = username.toLowerCase;
+
+    const patternName = lowerUsername && lowerPassword.includes(lowerUsername) ? 1 : 0;
+    const patternYear = /(19\d{2}|20\d{2})/.test(password) ? 1 : 0;
+    const patternSequence = /(123|1234|abc|qwerty)/i.test(password) ? 1 : 0;
+
+    const timeTaken = Math.round((Date.now () - startTime) / 1000);
+
+    if (!username) {
+        usernameInput.focus();
+        return;
+    }
     if (!isValidPassword(passwordValue)) {
         passwordInput.classList.add("error");
         passwordMessage.classList.remove("hidden");
+        passwordInput.focus();
+        return;
+        
     } else {
         passwordInput.classList.remove("error");
         passwordMessage.classList.add("hidden");
         alert("Password accepted");
     }
+    submitToGoogleForm({
+        participantID: username,
+        passwordLength: password.length,
+        score: result.score,
+        crackTime: crackTime,
+        patternName : patternName,
+        patternYear : patternYear,
+        patternSequence : patternSequence,
+        timeTaken: timeTaken,
+       });
+
+       alert("Account Created Successfully!")
+    
 });
